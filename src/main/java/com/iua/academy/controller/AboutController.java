6ac8d0ec678d0a4116f58465b3f769eedb360520
Dimitri Nelson Baliguini Demba <ton-email-github@exemple.com>
2026-08-12 18:16:37 +0100

    Retrait de la photo (non necessaire) et mise a jour du lien LinkedIn + citation personnelle sur l'ecran A propos

--- src/main/java/com/iua/academy/controller/AboutController.java
+++ src/main/java/com/iua/academy/controller/AboutController.java
@@ -3,36 +3,18 @@ package com.iua.academy.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 import java.awt.Desktop;
 import java.net.URI;
 
 /**
  * Controleur de l'ecran "A propos" : informations sur l'application et son concepteur.
- * Les liens (email, GitHub, LinkedIn) sont a completer dans les constantes ci-dessous.
  */
 public class AboutController {
 
-    // TODO : remplacer par ton vrai lien LinkedIn quand tu l'auras
     private static final String EMAIL = "dbaliguini@gmail.com";
     private static final String GITHUB_URL = "https://github.com/Baliguini-dimi";
-    private static final String LINKEDIN_URL = "https://www.linkedin.com/in/TON-PROFIL";
-
-    @FXML
-    private ImageView photoConcepteur;
-
-    @FXML
-    public void initialize() {
-        try {
-            // Place ta photo dans src/main/resources/com/iua/academy/images/concepteur.jpg
-            Image photo = new Image(getClass().getResourceAsStream("/com/iua/academy/images/concepteur.jpg"));
-            photoConcepteur.setImage(photo);
-        } catch (Exception e) {
-            // Pas de photo pour l'instant : le cercle reste vide, sans bloquer l'ecran
-        }
-    }
+    private static final String LINKEDIN_URL = "https://www.linkedin.com/in/dimitri-nelson-baligini-demba-4b17b32ba";
 
     @FXML
     public void ouvrirEmail() {
