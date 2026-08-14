8fa76d5e3b620143e632971185e1d9e7733f922d
Dimitri Nelson Baliguini Demba <ton-email-github@exemple.com>
2026-08-14 02:32:06 +0100

    Etape 10 : compilation finale, jar teste en conditions reelles, installateur Windows regenere avec la refonte UI complete

---
+++ src/main/resources/db/donnees_demo_enseignants.sql
@@ -0,0 +1,19 @@
+-- Mise a jour des enseignants par matiere (promo Master 1 IUA)
+
+UPDATE matiere SET enseignant = 'Dr Beman Kamagate' WHERE nom = 'Programmation systemes et reseaux sous Linux';
+UPDATE matiere SET enseignant = 'Dr Beman Kamagate' WHERE nom = 'Securite des systemes d''information';
+UPDATE matiere SET enseignant = 'Dr Armel Keupondo' WHERE nom = 'Technologies IP avancees';
+UPDATE matiere SET enseignant = 'M. Dje Bi Gabin' WHERE nom = 'PHP avance';
+UPDATE matiere SET enseignant = 'Dr Armel Keupondo' WHERE nom = 'Securite des reseaux';
+UPDATE matiere SET enseignant = 'Dr Beman Kamagate' WHERE nom = 'Programmation systemes et reseaux sous .NET';
+UPDATE matiere SET enseignant = 'Dr Angoran' WHERE nom = 'Droit et contractualisation dans l''informatique';
+UPDATE matiere SET enseignant = 'Dr Kopoin' WHERE nom = 'Developpement mobiles';
+UPDATE matiere SET enseignant = 'M. Dje Bi Gabin' WHERE nom = 'PHP et Framework associes';
+UPDATE matiere SET enseignant = 'Dr Moise Mambe' WHERE nom = 'Architectures logicielles et web services';
+UPDATE matiere SET enseignant = 'Dr Kanga Koffi' WHERE nom = 'Base de donnees avancees';
+UPDATE matiere SET enseignant = 'Dr Pandry' WHERE nom = 'Mathematiques du signal';
+UPDATE matiere SET enseignant = 'M. Ngbesso' WHERE nom = 'Administration Reseau';
+UPDATE matiere SET enseignant = 'Dr Beman Kamagate' WHERE nom = 'Apprentissage automatique';
+UPDATE matiere SET enseignant = 'M. Ngbesso' WHERE nom = 'Administration systeme';
+
+-- Methodes Agiles : non indique sur l'emploi du temps, reste "A definir"
